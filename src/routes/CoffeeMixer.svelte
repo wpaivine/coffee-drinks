6fed66dd4c8ed4657eb656f10dc58223506f9fcd
Bill Paivine <bill@paivine.com>
2022-10-31 00:46:16 -0400

    minor tweaks for mobile

--- src/routes/CoffeeMixer.svelte
+++ src/routes/CoffeeMixer.svelte
@@ -44,11 +44,11 @@
 </script>
 
 <div class="min-w-max w-1/2 m-auto">
-	<div class="grid grid-cols-4 gap-1 min-w-min ">
-		<div class="select-none">
-			<span class="float-right font-body text-brown-900 text-2xl px-4"> drink size </span>
+	<div class="grid grid-cols-5 gap-1 w-auto m-auto">
+		<div class="select-none col-span-2">
+			<span class="float-right font-body text-brown-900 text-2xl px-1"> drink size </span>
 		</div>
-		<div class="col-span-2">
+		<div class="col-span-2 ">
 			<input
 				class="float-left rounded-xl overflow-hidden appearance-none bg-brown-600 w-full drop-shadow-md"
 				type="range"
@@ -59,19 +59,19 @@
 			/>
 		</div>
 		<div>
-			<span class="float-left px-4 font-body text-brown-900 text-2xl">
+			<span class="float-left px-4 font-body text-brown-900 text-2xl ">
 				{drinkMass}g
 			</span>
 		</div>
 		{#each ingredientNames as coffeeIngredient, idx}
-			<div class="select-none">
-				<span class="float-right font-body text-brown-900 text-2xl select-none px-4">
+			<div class="select-none col-span-2">
+				<span class="float-right font-body text-brown-900 text-2xl select-none px-1">
 					{coffeeIngredient.name}
 				</span>
 			</div>
 			<div class="select-none col-span-2">
 				<input
-					class="ingredient float-left rounded-xl overflow-hidden appearance-none bg-brown-800 w-full drop-shadow-md"
+					class="ingredient float-right rounded-xl overflow-hidden appearance-none bg-brown-800 w-full drop-shadow-md"
 					type="range"
 					bind:value={sliderValues[idx]}
 					min="0"
@@ -88,14 +88,14 @@
 
 	<div class="grid place-items-center gap-5 py-10">
 		<CoffeeCup color={calculateColor(currentIngredients)} size={drinkMass} />
-		<span class="content-center font-body text-6xl text-brown-500">
+		<span class="content-center font-body text-5xl text-brown-500">
 			{#if closestDrink.distance < threshold}
 				{closestDrink.name}
 			{:else}
 				a hot drink
 			{/if}
 		</span>
-		<div class="grid grid-cols-4 gap-3">
+		<div class="grid grid-cols-2 gap-3">
 			{#each recipes as recipe}
 				{#if recipe.display}
 					<div
--- src/routes/types.ts
+++ src/routes/types.ts
@@ -6,16 +6,16 @@ export interface Recipe {
 
 export type Ingredients = [
 	espresso: number,
-	steamedMilk: number,
 	microfoam: number,
-	hotWater: number
+	hotWater: number,
+	steamedMilk: number,
 ];
 
 export const ingredientNames = [
 	{ name: 'espresso', c: 0, m: 40, y: 65, k: 82, alpha: 100 },
-	{ name: 'steamed milk', c: 3, m: 4, y: 8, k: 2, alpha: 30 },
 	{ name: 'microfoam', c: 2, m: 2, y: 2, k: 0, alpha: 20 },
-	{ name: 'hot water', c: 10, m: 0, y: 0, k: 0, alpha: 1 }
+	{ name: 'hot water', c: 10, m: 0, y: 0, k: 0, alpha: 1 },
+	{ name: 'steamed milk', c: 3, m: 4, y: 8, k: 2, alpha: 30 },
 ];
 
 export const makeIngredients = (obj: {
