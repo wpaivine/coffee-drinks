a527e07bcecfa8e04ff1b384c570e254530c578e
Bill Paivine <bill@paivine.com>
2022-11-01 19:02:44 -0400

    make grid responsive

--- src/routes/CoffeeMixer.svelte
+++ src/routes/CoffeeMixer.svelte
@@ -4,7 +4,6 @@
 		calculateClosestDrink,
 		calculateColor,
 		ingredientNames,
-		makeIngredients,
 		normalizeIngredients,
 		recipes,
 		type Recipe,
@@ -36,25 +35,16 @@
 			sliderValues[idx] = sliderValue;
 		});
 	}
-
-	/*
-<div style="visibility: visible">
-	<span>sliderValues: {sliderValues}</span><br />
-	<span>drinkMass: {drinkMass}</span><br />
-	<span>currentIngredients: {currentIngredients}</span><br />
-	<span>color: {calculateColor(currentIngredients)} </span><br />
-</div>
-    */
 </script>
 
 <div class="min-w-max w-1/2 m-auto">
 	<div
-		class="grid grid-cols-5 gap-1 w-auto m-auto bg-brown-200 rounded-md py-3 px-1 drop-shadow-md"
+		class="grid grid-cols-[10rem_auto_4rem] gap-1 w-auto m-auto bg-brown-200 rounded-md py-3 px-1 drop-shadow-md"
 	>
-		<div class="select-none col-span-2">
+		<div class="select-none">
 			<span class="float-right font-body text-brown-900 text-xl px-1"> drink size </span>
 		</div>
-		<div class="col-span-2 ">
+		<div>
 			<input
 				class="float-left rounded-xl overflow-hidden appearance-none bg-brown-600 w-full drop-shadow-md"
 				type="range"
@@ -70,12 +60,12 @@
 			</span>
 		</div>
 		{#each ingredientNames as coffeeIngredient, idx}
-			<div class="select-none col-span-2">
+			<div class="select-none">
 				<span class="float-right font-body text-brown-900 text-xl select-none px-1">
 					{coffeeIngredient.name}
 				</span>
 			</div>
-			<div class="select-none col-span-2">
+			<div class="select-none">
 				<input
 					class="ingredient float-right rounded-xl overflow-hidden appearance-none bg-brown-800 w-full drop-shadow-md"
 					type="range"
@@ -101,7 +91,7 @@
 				a hot drink
 			{/if}
 		</span>
-		<div class="grid grid-cols-2 gap-3">
+		<div class="grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 gap-3">
 			{#each recipes as recipe}
 				{#if recipe.display}
 					<div
